9de0b6aca18863a382ef341fd6a7bf677a126eb1
Kyle Hunter <kyle@thehunters.ca>
2025-09-08 09:29:06 -0700

    adds identities, lifecycle destroy policy

--- modules/secrets/main.tf
+++ modules/secrets/main.tf
@@ -6,7 +6,7 @@ locals {
     { name = "apps",               description = "Secrets used within apps",                                  version = 2 },
     { name = "private-team",       description = "Team secrets - only accessible by team members",            version = 2 },
     { name = "external",           description = "Secrets for services or stand-alone projects outside the LTC", version = 2 },
-    { name = "ltc-infrastructure", description = "Inventory and configuration secrets for LTC infrastructure", version = 2 },
+    { name = "ltc-infrastructure", description = "Inventory and configuration secrets", version = 2 },
   ]
 }
 
@@ -24,5 +24,5 @@ resource "vault_mount" "kv_mount" {
   options     = { version = tostring(each.value.version) }
 
   # Avoid nuking a mount (and all its data) via Terraform
-  # lifecycle { prevent_destroy = true }
+  lifecycle { prevent_destroy = true }
 }
